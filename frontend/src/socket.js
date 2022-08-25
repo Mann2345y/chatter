e16a24ade225a98912cbab60ce81b90f8625b5b3
Mahesh Arya <manish0075arya@gmail.com>
2022-08-25 10:49:03 +0530

    tryout

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 const SOCKET_URL = "https://chatterwebsapp.herokuapp.com/";
 
-export const socket = io(SOCKET_URL);
+export const socket = io(SOCKET_URL, { transports: ["websocket"] });
 
 socket.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
